e1da6ad44ab28435766b029f02f904efea5a03d5
KaitzkoScripts <100658638+KaitzkoScripts@users.noreply.github.com>
2022-02-28 20:33:18 -0800

    Update 99 Fail correct tile marker.lua

--- 99 Fail correct tile marker.lua
+++ 99 Fail correct tile marker.lua
@@ -1,6 +1,16 @@
 local m = game.Players.LocalPlayer:GetMouse()
 local uis = game:GetService("UserInputService")
+local vu = game:GetService("VirtualUser")
+local vuscript = nil
 local bruh = {}
+
+function notify(title, text)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = tostring(title),
+        Text = tostring(text),
+        Duration = 5
+    })
+end
 
 m.Button1Down:Connect(function()
     if (uis:IsKeyDown(Enum.KeyCode.LeftControl) and string.find(m.Target.Name, "Tile")) then
@@ -13,5 +23,22 @@ m.Button1Down:Connect(function()
         end
     elseif uis:IsKeyDown(Enum.KeyCode.Backspace) then
         m.Target.Color = Color3.fromRGB(198, 237, 255)
+    elseif uis:IsKeyDown(Enum.KeyCode.Period) then
+        notify("Recording", "Started recording")
+        game.Players.LocalPlayer.Character.HumanoidRootPart:PivotTo(CFrame.new(264.93808, 1995.86084, -147.131821, 0, 0, 1, 0, 1, 0, -1, 0, 0))
+        vu:StartRecording()
+    elseif uis:IsKeyDown(Enum.KeyCode.Return) then
+        notify("Recording", "Stopped recording")
+        vuscript = vu:StopRecording()
+        vuscript = string.gsub(tostring(vuscript), "SetKeUp", "SetKeyUp")
+        vuscript = string.gsub(tostring(vuscript), "wait", "task.wait")
+    elseif (uis:IsKeyDown(Enum.KeyCode.LeftBracket) or uis:IsKeyDown(Enum.KeyCode.RightBracket)) then
+        if vuscript == nil then
+            notify("No recording", "You have not recorded yet!")
+            return
+        else
+            game.Players.LocalPlayer.Character.HumanoidRootPart:PivotTo(CFrame.new(264.93808, 1995.86084, -147.131821, 0, 0, 1, 0, 1, 0, -1, 0, 0))
+            loadstring(tostring(vuscript))()
+        end
     end
 end)
