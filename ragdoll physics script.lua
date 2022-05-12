7dbd1f49ef508fa813f0aeefad7be9f7abae2dc8
KaitzkoScripts <100658638+KaitzkoScripts@users.noreply.github.com>
2022-05-11 20:50:01 -0700

    Create ragdoll physics script.lua

--- ragdoll physics script.lua
+++ ragdoll physics script.lua
@@ -1,9 +1,8 @@
 local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stebulous/solaris-ui-lib/main/source.lua"))()
 local helledppl = {}
 local picked = nil
--- local m = game.Players.LocalPlayer:GetMouse()
-
-math.randomseed(tick())
+local usingmouse = false
+local m = game.Players.LocalPlayer:GetMouse()
 
 function hell(player)
     if not player then
@@ -26,60 +25,45 @@ function hell(player)
 end
 
 local win = SolarisLib:New({
-    Name = "Ragdoll Physics",
-    FolderToSave = "RP_Stuff"
+    Name = "Ragdoll Physics"
 })
 local tab = win:Tab("Fun")
 local sec = tab:Section("Have fun trolling")
 local lab = sec:Label("Picked Player: None")
-local viewplr = sec:Button("View picked player", function()
-    workspace.CurrentCamera.CameraSubject = game.Players[picked].Character.Humanoid
-end)
-local unview = sec:Button("Unview", function()
-    workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
-end)
-local rand = sec:Button("Pick random player", function()
-    local randpick = game.Players:GetPlayers()[math.random(1, #game.Players:GetPlayers())].Name
-    picked = randpick
-    lab:Set("Picked Player: "..randpick)
-    SolarisLib:Notification("Picked", "Random player: "..randpick)
-end)
-local drpdown = sec:Dropdown("Players", {""}, "", "Dropdown", function(t)
+local drpdown = sec:Dropdown("Players", {"Refresh players!"}, "", "Dropdown", function(t)
     picked = t
-    lab:Set("Picked Player: "..t)
+    lab:Set(t)
 end)
-local bruh = {}
-for _,v in pairs(game.Players:GetPlayers()) do
-    table.insert(bruh, v.Name)
-end
-drpdown:Refresh(bruh, true)
 sec:Button("Refresh Players", function()
     local bruh = {}
     for _,v in pairs(game.Players:GetPlayers()) do
         table.insert(bruh, v.Name)
     end
-    drpdown:Refresh(bruh, true)
+    drpdown:Set(bruh)
 end)
 sec:Button("Fling Picked Player", function()
-    task.spawn(hell, game.Players[picked])
-end)
-
---[[ 
-m.Button1Down:Connect(function()
-    local plr = game.Players:GetPlayerFromCharacter(m.Target:FindFirstAncestorOfClass("Model"))
-    if plr then
-        task.spawn(hell, plr)
+    if picked == nil then
+        SolarisLib:Notification("Pick somebody", "Pick somebody first")
+    else
+        task.spawn(hell, game.Players[picked])
     end
 end)
-]]--
+sec:Toggle("Use mouse to pick", false, "Toggle", function(goofy)
+    usingmouse = goofy
+end)
+
+m.Button1Down:Connect(function()
+    if usingmouse then
+        local ratio = game.Players:GetPlayerFromCharacter(m.Target:FindFirstAncestorOfClass("Model"))
+        if ratio then
+            picked = ratio.Name
+            lab:Set("Picked Player: "..ratio.Name)
+        end
+    end
+end)
 
 game.Players.PlayerAdded:Connect(function(zaplayer)
     if table.find(helledppl, zaplayer) then
         task.spawn(hell, plr)
     end
 end)
-
-coroutine.wrap(function()
-    task.wait(5)
-    delfolder("/RP_Stuff")
-end)()
